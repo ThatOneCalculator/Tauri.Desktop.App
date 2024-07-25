2c5b8ca8bec9c6e1cb241eec8717cdbecc912a18
ThatOneCalculator <kainoa@t1c.dev>
2024-07-25 11:17:43 -0700

    fix compilation errors

--- src/components/settingsView/serverList.tsx
+++ src/components/settingsView/serverList.tsx
@@ -1,22 +1,30 @@
-import { useState } from "react"
-import "./style.css"
-import { ServerItem } from "./serverItem"
-import { RCServer } from "."
+import { useState } from "react";
+// import "./style.css"
+import { ServerItem } from "./serverItem";
+import type { RCServer } from ".";
 
 type ServerListProps = {
-    servers: RCServer[];
-    makeDefaultServer: (index: number, serverUrl: string) => void;
-    removeServer: (index: number) => void;
-}
+	servers: RCServer[];
+	makeDefaultServer: (index: number, serverUrl: string) => void;
+	removeServer: (index: number) => void;
+};
 
-export const ServerList = ({ servers, makeDefaultServer, removeServer } : ServerListProps) => {
-
-    return (
-        <div className="serversList">
-            {
-                servers.map((server, index) => <ServerItem index={index} server={server} makeDefaultServer={makeDefaultServer} removeServer={removeServer}/>)
-            }
-        </div>
-    )
-}
-
+export const ServerList = ({
+	servers,
+	makeDefaultServer,
+	removeServer,
+}: ServerListProps) => {
+	return (
+		<div className="serversList">
+			{servers.map((server, index) => (
+				<ServerItem
+					key={server.serverUrl}
+					index={index}
+					server={server}
+					makeDefaultServer={makeDefaultServer}
+					removeServer={removeServer}
+				/>
+			))}
+		</div>
+	);
+};
--- src/shell/index.tsx
+++ src/shell/index.tsx
@@ -1,39 +1,42 @@
-import DownloadManager from '../components/downloadManagerView';
-import { isPermissionGranted, requestPermission, sendNotification } from '@tauri-apps/api/notification';
-import ServerView from '../components/openServerView';
-import Settings from '../components/settingsView';
-import Sidebar from '../components/Sidebar';
-import { GlobalStyles, Wrapper, WindowDragBar, ViewsWrapper } from './styles';
-import { useEffect } from 'react';
+import DownloadManager from "../components/downloadManagerView";
+import {
+	isPermissionGranted,
+	requestPermission,
+	sendNotification,
+} from "@tauri-apps/api/notification";
+import ServerView from "../components/openServerView";
+import Settings from "../components/settingsView";
+import Sidebar from "../components/sidebar";
+import { GlobalStyles, Wrapper, WindowDragBar, ViewsWrapper } from "./styles";
+import { useEffect } from "react";
 
 export const Shell = () => {
+	const confirmNotificationPermission = async () => {
+		let permissionGranted = await isPermissionGranted();
+		if (!permissionGranted) {
+			const permission = await requestPermission();
+			permissionGranted = permission === "granted";
+		}
+		if (permissionGranted) {
+			console.log("Permission Granted");
+			sendNotification("Tauri is awesome!");
+			sendNotification({ title: "TAURI", body: "Tauri is awesome!" });
+		}
+	};
 
-  const confirmNotificationPermission = async () => {
-    let permissionGranted = await isPermissionGranted();
-    if (!permissionGranted) {
-      const permission = await requestPermission();
-      permissionGranted = permission === 'granted';
-    }
-    if (permissionGranted) {
-      console.log("Permission Granted")
-      sendNotification('Tauri is awesome!');
-      sendNotification({ title: 'TAURI', body: 'Tauri is awesome!' });
-    }
-  }
+	(async () => await confirmNotificationPermission())();
 
-  (async() => await confirmNotificationPermission())()
-
-  return (
-    <>
-      <GlobalStyles />
-      <WindowDragBar />
-      <Wrapper>
-        <Sidebar />
-        <ViewsWrapper>
-          <ServerView />
-          <DownloadManager />
-        </ViewsWrapper>
-      </Wrapper>
-    </>
-  );
-}
+	return (
+		<>
+			<GlobalStyles />
+			<WindowDragBar />
+			<Wrapper>
+				<Sidebar />
+				<ViewsWrapper>
+					<ServerView />
+					<DownloadManager />
+				</ViewsWrapper>
+			</Wrapper>
+		</>
+	);
+};
